c0db43b6b5d0de2b37fc9618876c0ebf99d02165
Joe McConnell <joe.mcconnell10@gmail.com>
2023-09-04 15:40:18 -0500

    prints a simple grid

---
+++ sodoku.py
@@ -0,0 +1,22 @@
+# N is the size of the 2D matrix   N*N
+N = 9
+ 
+# A utility function to print grid
+def printing(arr):
+    for i in range(N):
+        for j in range(N):
+            print(arr[i][j], end = " ")
+        print()
+
+# 0 means unassigned cells
+grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+        [5, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 8, 7, 0, 0, 0, 0, 3, 1],
+        [0, 0, 3, 0, 1, 0, 0, 8, 0],
+        [9, 0, 0, 8, 6, 3, 0, 0, 5],
+        [0, 5, 0, 0, 9, 0, 6, 0, 0],
+        [1, 3, 0, 0, 0, 0, 2, 5, 0],
+        [0, 0, 0, 0, 0, 0, 0, 7, 4],
+        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+printing(grid)
